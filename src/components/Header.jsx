ad1263a7339754a97d83fd1dbb6a7cec5bb9db8b
FinExWeb <akhmedovweb@gmail.com>
2023-07-17 10:52:25 +0500

    add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import NavbarMenu from './Navbar/Navbar'
 import HeaderApp from './Header/HeaderApp'
+import Card from './Card/Card'
+import './Header.css'
 function Header() {
     return (
         <div className='container'>
@@ -10,6 +12,11 @@ function Header() {
             <div className="header">
                 <HeaderApp />
             </div>
+            <hr />
+            <div className="foods my-5">
+                <Card />
+            </div>
+            <hr className='my-2' />
         </div>
     )
 }
